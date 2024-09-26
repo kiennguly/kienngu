7b8f555dfd3c53108cfce69bce42cbced0b249e8
kiennguly <24520885@gm.uit.edu.vn>
2024-09-26 20:18:43 +0700

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,31 @@
+﻿// bài tập về con cặc lỏ của thầy kiên
+#include <iostream>
+
+class ArrayCalculator {
+public:
+	static int sumOfArray(int arr[], int n) {
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+
+	static double sumOfArray(double arr[], int n) {
+		double sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+};
+
+using namespace std;
+
+int main() {
+	int arr1[] = { 3, 4, 2 };
+	double arr2[] = { 1.3, 4.2, 6.7 };
+	cout << ArrayCalculator::sumOfArray(arr1, sizeof(arr1) / sizeof(int)) << endl;
+	cout << ArrayCalculator::sumOfArray(arr2, sizeof(arr2) / sizeof(double)) << endl;
+	return 0;
+}
